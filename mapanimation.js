fddd59a2c53fca3f6c9d9372c7011f33f4fbfa90
23h1 <nair.abhijeet@gmail.com>
2022-09-20 02:37:11 +0100

    Refactored code

--- mapanimation.js
+++ mapanimation.js
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>Add a default marker</title>
-<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-<script src="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js"></script>
-<link href="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css" rel="stylesheet" />
-<style>
-  body { margin: 0; padding: 0; }
-  #map { position: absolute; top: 0; bottom: 0; width: 100%; }
 
-  .map-overlay{
-    position: absolute;
-    left: 0;
-    padding: 10px;
-  }
-</style>
-</head>
-
-<body>
-
-<div id="map"></div>
-<div class="map-overlay top">
-  <button style="font-size: 2em;" onclick="move()"> 
-    Show stops between Bourenmouth station & Bournemouth Uni  
-  </button>
-</div>
-
-
-<script>
     mapboxgl.accessToken = 'pk.eyJ1IjoiMjNoMSIsImEiOiJjbDdwYTU0bHIwZnFnM3JzM2ViY2NhMGxvIn0.AeJkVXbm4lhCJkImsQomFQ';
     
     var map = new mapboxgl.Map({
@@ -85,6 +55,3 @@
         // mapbox://styles/mapbox/dark-v10
         // mapbox://styles/mapbox/satellite-v9
         // mapbox://styles/mapbox/satellite-streets-v11
-</script>
-</body>
-</html>
